aed0f30a4898b7b89d76f2212b6d153a484ee7d8
Ayuma <adityajatisari@gmail.com>
2026-01-24 13:13:23 +0700

    Adding Debuff for MC, method in Global Script

--- Main/global.gd
+++ Main/global.gd
@@ -4,7 +4,11 @@ var McHealth: int = 100
 var CanCharMove :bool = true
 var Player: CharacterBody2D
 var TotalEnemy: int = 0
-   
+var slow_mov: float = 1
+var mov_push: Vector2 = Vector2(0,0)
+var knocback_pow: float = 0   
+var knockback_direction: Vector2 = Vector2(0,0)
+
 func _ready() -> void:
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
 	
@@ -18,9 +22,20 @@ func change_scene(path: String):
 func McDeath():
 	print("Death")
 	pass
-	#var node = "res://Main/Camera.tscn"
+	#var node = load("res://Main/Camera.tscn")
 	#get_tree().current_scene.add_child(node)
+func char_stun(duration: float):
+	self.CanCharMove = false
+	await get_tree().create_timer(duration).timeout
+	self.CanCharMove = true
+	
+func MovePush(amounts: Vector2):
+	self.mov_push = amounts
+	
 func take_damage(amount: int):
 	self.McHealth -= amount
 	if self.McHealth <= 0:
 		self.McDeath()
+func McKnockBack(power: float, direction:Vector2):
+	self.knocback_pow = power
+	self.knockback_direction = direction
