b1d9b25d975ba4ffdf8d94add00fc9f47df95f6d
ariear <arieinjector@gmail.com>
2026-01-27 11:20:01 +0700

    feat(add): leg boss mechanic

--- StageKaki/boss_leg.gd
+++ StageKaki/boss_leg.gd
@@ -1,17 +1,33 @@
 extends CharacterBody2D
 
 @onready var anim = $AnimatedSprite2D
+@onready var stomp_hitbox = $StompHitbox
+@onready var MudAreaScene = preload("res://StageKaki/mud_area.tscn")
 
-@export var move_speed := 60.0
-
+@export var move_speed := 55.0
 @export var jump_trigger_distance := 220.0
 @export var jump_duration := 0.5
-@export var stomp_cooldown_time := 2.0
+@export var stomp_cooldown_time := 14
+@export var stomp_y_offset := 42.0
+var is_paused := false
+@export var post_stomp_pause := 1.2
 
-enum {
-	CHASE,
-	STOMP
-}
+@export var stomp_damage := 15
+@export var stomp_knockback_power := 0.2
+
+@onready var basic_hitbox = $BasicHitbox
+
+@export var basic_attack_distance := 65.0
+@export var basic_attack_damage := 5
+@export var basic_attack_count := 3
+@export var basic_attack_interval := 0.35
+
+var can_stomp := true
+
+@export var mud_chance := 0.4
+@export var mud_pause_time := 1.0
+
+enum { CHASE, STOMP, BASIC, MUD }
 var state = CHASE
 
 var jump_start_pos: Vector2
@@ -19,10 +35,24 @@ var jump_target_pos: Vector2
 var jump_timer := 0.0
 var stomp_cooldown := false
 
+var player_was_outside_stomp_range := true
+
 func _ready():
 	state = CHASE
+	stomp_hitbox.monitoring = false
+	basic_hitbox.monitoring = false
 
 func _physics_process(delta):
+	print("HP PLAYER:", Global.McHealth)
+	if is_paused:
+			velocity = Vector2.ZERO
+			move_and_slide()
+			return
+	if Global.McHealth <= 0:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
+
 	if Global.Player == null:
 		return
 
@@ -33,57 +63,125 @@ func _physics_process(delta):
 
 		STOMP:
 			process_stomp(delta)
+		BASIC:
+			pass
 
 func chase_player():
-	var dir = Global.Player.global_position - global_position
-	dir = dir.normalized()
-
+	var dir = (Global.Player.global_position - global_position).normalized()
 	velocity = dir * move_speed
 	move_and_slide()
 
 	if abs(dir.x) > abs(dir.y):
-		if dir.x > 0:
-			anim.play("walk_right")
-		else:
-			anim.play("walk_left")
+		anim.play("walk_right" if dir.x > 0 else "walk_left")
 	else:
-		if dir.y > 0:
-			anim.play("walk_down")
-		else:
-			anim.play("walk_up")
+		anim.play("walk_down" if dir.y > 0 else "walk_up")
 
 func check_player_distance():
-	if stomp_cooldown or state == STOMP:
+	if state != CHASE:
 		return
 
 	var dist = global_position.distance_to(Global.Player.global_position)
 
-	if dist <= jump_trigger_distance:
+	var player_inside_stomp_range = dist <= jump_trigger_distance
+
+	if player_inside_stomp_range \
+	and player_was_outside_stomp_range \
+	and not stomp_cooldown:
 		enter_stomp()
+		player_was_outside_stomp_range = false
+		return
+
+	if dist <= basic_attack_distance:
+		if randf() < mud_chance:
+			enter_mud_attack()
+		else:
+			enter_basic_attack()
+		return
+
+	if not player_inside_stomp_range:
+		player_was_outside_stomp_range = true
+
+
+func spawn_mud():
+	var mud = MudAreaScene.instantiate()
+	get_parent().add_child(mud)
+
+	mud.global_position = global_position
+
+func enter_mud_attack():
+	state = MUD
+	is_paused = true
+	velocity = Vector2.ZERO
+
+	spawn_mud()
+
+	await get_tree().create_timer(mud_pause_time).timeout
+
+	is_paused = false
+	state = CHASE
+
+
+func enter_basic_attack():
+	state = BASIC
+	is_paused = true
+	velocity = Vector2.ZERO
+
+	await basic_attack_sequence()
+
+	is_paused = false
+	state = CHASE
+
+func basic_attack_sequence():
+	for i in range(basic_attack_count):
+		anim.play("basic_attack")
+
+		basic_hitbox.monitoring = true
+		await get_tree().physics_frame
+
+		if basic_hitbox.overlaps_body(Global.Player):
+			Global.McKnockBack(0.05, global_position)
+			Global.take_damage(basic_attack_damage)
+
+		basic_hitbox.monitoring = false
+		await get_tree().create_timer(basic_attack_interval).timeout
 
 func enter_stomp():
 	state = STOMP
+	can_stomp = false
 	jump_timer = 0.0
 	velocity = Vector2.ZERO
 
 	jump_start_pos = global_position
 	jump_target_pos = Global.Player.global_position
+	jump_target_pos.y -= stomp_y_offset
 
 	anim.play("stomp")
 
 func process_stomp(delta):
 	jump_timer += delta
-	var t = jump_timer / jump_duration
-	t = clamp(t, 0.0, 1.0)
-
+	var t = clamp(jump_timer / jump_duration, 0.0, 1.0)
 	global_position = jump_start_pos.lerp(jump_target_pos, t)
 
 	if t >= 1.0:
+		state = CHASE
 		land_after_stomp()
 
 func land_after_stomp():
-	state = CHASE
-	stomp_cooldown = true
+	stomp_hitbox.monitoring = true
 
+	await get_tree().physics_frame
+
+	if stomp_hitbox.overlaps_body(Global.Player):
+		Global.McKnockBack(stomp_knockback_power, global_position)
+		Global.take_damage(stomp_damage)
+
+	stomp_hitbox.monitoring = false
+	
+	is_paused = true
+	anim.play("idle")
+	await get_tree().create_timer(post_stomp_pause).timeout
+	is_paused = false
+
+	stomp_cooldown = true
 	await get_tree().create_timer(stomp_cooldown_time).timeout
 	stomp_cooldown = false
