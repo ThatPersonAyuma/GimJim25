42a52d74197794581e049e3cbea457b95d40f411
HarmlessValve <t4j42007@gmail.com>
2026-01-28 22:27:01 +0700

    [+] Final Boss and gonna fix collision Body Entered later

--- StageFinal/bossFinal.gd
+++ StageFinal/bossFinal.gd
@@ -1,31 +1,35 @@
 extends CharacterBody2D
 
 @onready var anim = $AnimatedSprite2D
-@onready var smokes = $smokes
 @onready var  NearbyAttackRange = $NA_range
 @onready var shoot_point = $ShootPoint
+
 
 @export var knockback_dmg: int = 10
 @export var speed = 50.0
 @export var max_health = 50
+@export var mud_pause_time := 2.0
+@export var mud_cooldown := 6.0
+@export var pseudo_dash_multiplier := 6.0
+@export var pseudo_dash_duration := 0.08
+@export var pseudo_dash_cooldown := 0.25
 
-var waves_path = preload("res://StageKepala/bullet.tscn")
 var bullet_path = preload("res://StageKepala/bullet2.tscn")
 var laser_path = preload("res://StageKepala/laser.tscn")
-var pool_path = preload("res://StageKepala/pool.tscn")
+var MudAreaScene = preload("res://StageKaki/mud_area.tscn")
 
 var can_shoot_bullets= true
-var can_shoot_waves = true
 var can_shoot_laser = true
-var can_spawn_pool := true
 var shoot_cooldown_bullets = 0.8
-var shoot_cooldown_waves = 3
 var shoot_cooldown_lasers = 15
-var pool_cooldown := 5.0
+var can_spawn_mud := true
+var mud_chance := 0.4
 
 var HP : int
 var is_mc_in_range = false
 var can_attack: bool = true
+var can_pseudo_dash := true
+var is_paused := false
 var state = "idle"
 var phase_2 = false
 
@@ -35,7 +39,6 @@ func _ready() -> void:
 	
 	NearbyAttackRange.connect("body_entered", body_entered)
 	NearbyAttackRange.connect("body_exited", body_exited)
-	self.remove_child(smokes)
 	
 	anim.animation_finished.connect(_on_animation_finished)
 
@@ -46,17 +49,22 @@ func _physics_process(_delta):
 	if Global.McHealth <= 0:
 		velocity = Vector2.ZERO
 		return
+		
+	if is_paused:
+		velocity = Vector2.ZERO
+		return
 
 	match state:
 		"move":
 			DetectPlayer()
 			move_and_slide()
-			fire_waves()
 			
-			if phase_2:
+			if not phase_2:
+				try_spawn_mud()
 				fire_bullet()
+				
+			if phase_2:
 				try_fire_laser()
-				try_spawn_pool()
 		"idle":
 			velocity = Vector2.ZERO
 		"knockback":
@@ -81,30 +89,30 @@ func _on_animation_finished():
 	if state == "move":
 		anim.play("walk")
 
-func DetectPlayer():
-		var target_pos = Global.Player.global_position
-		velocity = global_position.direction_to(target_pos) * speed
+func dash_flash():
+	anim.modulate = Color(1, 1, 1, 0.3) # transparan
+	await get_tree().create_timer(0.05).timeout
+	anim.modulate = Color.WHITE
 
-func fire_waves():
-	if not can_shoot_waves:
+func DetectPlayer():
+	if not can_pseudo_dash:
+		return
+	if not Global.Player:
 		return
 
-	can_shoot_waves = false
+	can_pseudo_dash = false
 
-	var bullet = waves_path.instantiate()
-	get_parent().add_child(bullet)
+	var dir = (Global.Player.global_position - global_position).normalized()
 
-	bullet.global_position = shoot_point.global_position
+	dash_flash() # FLASH DI SINI
 
-	if Global.Player:
-		var dir = (Global.Player.global_position - shoot_point.global_position).normalized()
-		bullet.set_direction(dir)
+	velocity = dir * speed * pseudo_dash_multiplier
 
-	print("bullet fired")
-	anim.animation = "fire_waves"
-	anim.play()
-	await get_tree().create_timer(shoot_cooldown_waves).timeout
-	can_shoot_waves = true
+	await get_tree().create_timer(pseudo_dash_duration).timeout
+	velocity = Vector2.ZERO
+
+	await get_tree().create_timer(pseudo_dash_cooldown).timeout
+	can_pseudo_dash = true
 
 func fire_bullet():
 	if not can_shoot_bullets:
@@ -149,7 +157,7 @@ func fire_laser():
 
 		laser.sweep_dir = sweep_dir
 
-	anim.animation = "fire_waves"
+	anim.animation = "fire_laser"
 	anim.play()
 
 	await get_tree().create_timer(2.0).timeout
@@ -165,30 +173,42 @@ func try_fire_laser():
 		return
 	fire_laser()
 
-func spawn_water_pool():
-	can_spawn_pool = false
+func spawn_mud():
+	can_spawn_mud = false
 
-	var pool = pool_path.instantiate()
-	get_parent().add_child(pool)
-	pool.global_position = global_position + Vector2(0, 24)
+	var mud = MudAreaScene.instantiate()
+	mud.z_index = -1
+	get_parent().add_child(mud)
+	mud.global_position = global_position + Vector2(0, 24)
 
-	await get_tree().create_timer(pool_cooldown).timeout
-	can_spawn_pool = true
+	enter_mud_attack()
 
-func try_spawn_pool():
-	if not phase_2:
+	await get_tree().create_timer(mud_cooldown).timeout
+	can_spawn_mud = true
+
+func enter_mud_attack():
+	is_paused = true
+	velocity = Vector2.ZERO
+
+	await get_tree().create_timer(mud_pause_time).timeout
+
+	is_paused = false
+	
+func try_spawn_mud():
+	if phase_2:
 		return
-	if not can_spawn_pool:
+	if not can_spawn_mud:
 		return
 
-	spawn_water_pool()
+	spawn_mud()
+
 
 func OnKnockback(player):
 	state = "knockback"
 	Global.take_damage(knockback_dmg)
 
 	var knockback_dir = (global_position - player.global_position).normalized()
-	velocity = knockback_dir * 125.0
+	velocity = knockback_dir * 150.0
 
 	await get_tree().create_timer(0.5).timeout
 	OnMove()
@@ -219,11 +239,6 @@ func take_damage(amount):
 func enter_phase_2():
 	phase_2 = true
 	speed = 55
-	self.remove_child(anim)
-	self.add_child(smokes)
-	self.add_child(anim)
-	smokes.animation = "smokes"
-	smokes.play()
 
 func die():
 	if Global.Enemy == self:
