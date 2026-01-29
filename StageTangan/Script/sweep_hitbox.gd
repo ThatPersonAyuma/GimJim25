87b8f247f8b1426ba4ffc3657337c9c4f9735245
ariear <arieinjector@gmail.com>
2026-01-29 14:37:52 +0700

    feat(add): sfx on stagetangan

--- StageTangan/Script/sweep_hitbox.gd
+++ StageTangan/Script/sweep_hitbox.gd
@@ -10,45 +10,45 @@ var has_hit: bool = false
 @onready var collision_polygon = $CollisionPolygon2D
 
 func _ready():
-    print("Sweep hitbox spawned!")
-    
-    _create_arc_polygon()
-    
-    body_entered.connect(_on_body_entered)
-    
-    get_tree().create_timer(lifetime).timeout.connect(_on_expire)
+	print("Sweep hitbox spawned!")
+	
+	_create_arc_polygon()
+	
+	body_entered.connect(_on_body_entered)
+	
+	get_tree().create_timer(lifetime).timeout.connect(_on_expire)
 
 func _create_arc_polygon():
-    var points: PackedVector2Array = PackedVector2Array()
-    var segments = 12 
-    var half_angle = deg_to_rad(arc_angle / 2.0)
-    
-    points.append(Vector2.ZERO)
-    
-    for i in range(segments + 1):
-        var angle = -half_angle + (i * (half_angle * 2.0) / segments)
-        var point = Vector2(cos(angle), sin(angle)) * arc_radius
-        points.append(point)
-    
-    if collision_polygon:
-        collision_polygon.polygon = points
+	var points: PackedVector2Array = PackedVector2Array()
+	var segments = 12 
+	var half_angle = deg_to_rad(arc_angle / 2.0)
+	
+	points.append(Vector2.ZERO)
+	
+	for i in range(segments + 1):
+		var angle = -half_angle + (i * (half_angle * 2.0) / segments)
+		var point = Vector2(cos(angle), sin(angle)) * arc_radius
+		points.append(point)
+	
+	if collision_polygon:
+		collision_polygon.polygon = points
 
 func _on_body_entered(body):
-    if has_hit:
-        return
-    
-    if body == Global.Player:
-        has_hit = true
-        print("SWEEP HIT! Dealing ", damage, " damage")
-        
-        if Global.McHealth > 0:
-            Global.take_damage(damage)
-            # Knockback power relatif terhadap knockback_raw_pow dari mc.gd
-            var kb_multiplier = knockback_power / Global.Player.knockback_raw_pow
-            Global.McKnockBack(kb_multiplier, global_position)
-        
-        queue_free()
+	if has_hit:
+		return
+	
+	if body == Global.Player:
+		has_hit = true
+		print("SWEEP HIT! Dealing ", damage, " damage")
+		
+		if Global.McHealth > 0:
+			Global.take_damage(damage)
+			# Knockback power relatif terhadap knockback_raw_pow dari mc.gd
+			var kb_multiplier = knockback_power / Global.Player.knockback_raw_pow
+			Global.McKnockBack(kb_multiplier, global_position)
+		
+		queue_free()
 
 func _on_expire():
-    print("Sweep hitbox expired")
-    queue_free()
+	print("Sweep hitbox expired")
+	queue_free()
